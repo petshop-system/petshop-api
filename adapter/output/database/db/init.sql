7da7b315354255d4c65665bf981e58c0812c5f45
MaironCosta <maironmendes@yahoo.com.br>
2023-12-10 21:25:26 -0300

    Banco de dados
    iniciando estrutura de agendamento e contrato

--- adapter/output/database/db/init.sql
+++ adapter/output/database/db/init.sql
@@ -4,10 +4,10 @@ create schema petshop_api
 
     create table address
     (
-        id         serial       not null
+        id     serial       not null
             constraint petshop_api_address_pkey primary key,
         street varchar(255) not null,
-        number     varchar(255) not null
+        number varchar(255) not null
     )
 
     create
@@ -16,9 +16,9 @@ create schema petshop_api
 
     create table person
     (
-        id serial not null
+        id          serial       not null
             constraint petshop_api_person_pkey primary key,
-        document varchar(255) not null unique ,
+        document    varchar(255) not null unique,
         person_type varchar(255) not null
     )
 
@@ -28,13 +28,13 @@ create schema petshop_api
 
     create table contract
     (
-        id             serial       not null
-            constraint petshop_api_customer_pkey primary key,
-        name           varchar(255) not null,
-        email          varchar(255) not null unique ,
+        id            serial       not null
+            constraint petshop_api_contract_pkey primary key,
+        name          varchar(255) not null,
+        email         varchar(255) not null unique,
         date_created  timestamp default timezone('BRT'::text, now()),
-        fk_id_address int          not null unique ,
-        fk_id_person   int          not null unique ,
+        fk_id_address int          not null unique,
+        fk_id_person  int          not null unique,
         FOREIGN KEY (fk_id_address) references address (id),
         FOREIGN KEY (fk_id_person) references person (id)
     )
@@ -48,11 +48,11 @@ create schema petshop_api
         id             serial       not null
             constraint petshop_api_customer_pkey primary key,
         name           varchar(255) not null,
-        email          varchar(255) not null unique ,
-        date_created  timestamp default timezone('BRT'::text, now()),
-        fk_id_address int          not null unique ,
-        fk_id_person   int          not null unique ,
-        fk_id_contract   int          not null ,
+        email          varchar(255) not null unique,
+        date_created   timestamp default timezone('BRT'::text, now()),
+        fk_id_address  int          not null unique,
+        fk_id_person   int          not null unique,
+        fk_id_contract int          not null,
         FOREIGN KEY (fk_id_contract) references contract (id),
         FOREIGN KEY (fk_id_address) references address (id),
         FOREIGN KEY (fk_id_person) references person (id)
@@ -64,12 +64,12 @@ create schema petshop_api
 
     create table phone
     (
-        id            serial       not null
+        id           serial       not null
             constraint petshop_api_phone_pkey primary key,
-        number        varchar(255) not null,
-        location           varchar(255) not null,
-        phone_type varchar(255) not null,
-        fk_id_person  int          not null,
+        number       varchar(255) not null,
+        location     varchar(255) not null,
+        phone_type   varchar(255) not null,
+        fk_id_person int          not null,
         FOREIGN KEY (fk_id_person) references person (id)
     )
 
@@ -103,14 +103,14 @@ create schema petshop_api
 
     create table pet
     (
-        id              serial       not null
+        id             serial       not null
             constraint petshop_api_pet_pkey primary key,
-        name            varchar(255) not null,
+        name           varchar(255) not null,
         date_created   timestamp default timezone('BRT'::text, now()),
-        date_birthday date      default timezone('BRT'::text, null),
-        fk_id_customer   int          not null,
-        fk_id_breed      int          not null,
-        fk_id_contract   int          not null ,
+        date_birthday  date      default timezone('BRT'::text, null),
+        fk_id_customer int          not null,
+        fk_id_breed    int          not null,
+        fk_id_contract int          not null,
         FOREIGN KEY (fk_id_contract) references contract (id),
         FOREIGN KEY (fk_id_customer) references customer (id),
         FOREIGN KEY (fk_id_breed) references breed (id)
@@ -118,16 +118,51 @@ create schema petshop_api
 
     create
         unique index petshop_api_pet_id_uindex
-        on pet (id);
+        on pet (id)
 
---     create table schedule (
---                               id              serial       not null
---                                   constraint petshop_api_schedule_pkey primary key,
---     )
---
---     create
---         unique index petshop_api_schedule_id_uindex
---         on schedule (id);
+    create table service
+    (
+        id    serial       not null
+            constraint petshop_api_service_pkey primary key,
+        name  varchar(255) not null,
+        price decimal      not null default 0
+    )
+
+    create
+        unique index petshop_api_service_id_uindex
+        on service (id)
+
+    create table service_time
+    (
+        id            serial       not null
+            constraint petshop_api_service_time_pkey primary key,
+        hour          varchar(255) not null,
+        fk_id_service int          not null,
+        FOREIGN KEY (fk_id_service) references service (id)
+    )
+
+    create
+        unique index petshop_api_service_time_id_uindex
+        on service_time (id)
+
+    create table schedule
+    (
+        id                 serial  not null
+            constraint petshop_api_schedule_pkey primary key,
+        date_created       timestamp        default timezone('BRT'::text, now()),
+        booking            date    not null,
+        price              decimal not null default 0,
+        fk_id_pet          int     not null,
+        fk_id_service_time int     not null,
+        fk_id_contract     int     not null,
+        FOREIGN KEY (fk_id_contract) references contract (id),
+        FOREIGN KEY (fk_id_pet) references pet (id),
+        FOREIGN KEY (fk_id_service_time) references service_time (id)
+    )
+
+    create
+        unique index petshop_api_schedule_id_uindex
+        on schedule (id);
 
 
 -- Create default clientes inserts
@@ -138,14 +173,22 @@ VALUES ('Rua Jose Bonifácio', 1432);
 INSERT INTO petshop_api.address (street, number)
 VALUES ('Rua Lechitz', 11);
 
-INSERT INTO petshop_api.person (document, person_type) VALUES ('22233344409', 'individual');
-INSERT INTO petshop_api.person (document, person_type) VALUES ('38988657000181', 'legal');
+INSERT INTO petshop_api.address (street, number)
+VALUES ('Av. Juiz de Fora', 1001);
 
-INSERT INTO petshop_api.customer (name, fk_id_address, email, date_created, fk_id_person)
-VALUES ('siclano', 1, 'siclano@gmail.com', now(), 1);
+INSERT INTO petshop_api.person (document, person_type)
+VALUES ('22233344409', 'individual');
+INSERT INTO petshop_api.person (document, person_type)
+VALUES ('38988657000181', 'legal');
 
-INSERT INTO petshop_api.customer (name, fk_id_address, email, date_created, fk_id_person)
-VALUES ('testando cnpj', 2, 'company@gmail.com', now(), 2);
+INSERT INTO petshop_api.contract (name, email, date_created, fk_id_address, fk_id_person)
+VALUES ('petshop juiz de fora', 'pet_jf@gmail.com', now(), 3, 2) ;
+
+INSERT INTO petshop_api.customer (name, fk_id_address, email, date_created, fk_id_person, fk_id_contract)
+VALUES ('siclano', 1, 'siclano@gmail.com', now(), 1, 1);
+
+INSERT INTO petshop_api.customer (name, fk_id_address, email, date_created, fk_id_person, fk_id_contract)
+VALUES ('testando cnpj', 2, 'company@gmail.com', now(), 2, 1);
 
 INSERT INTO petshop_api.phone (number, location, phone_type, fk_id_person)
 VALUES ('912345678', '72', 'celular', 1);
@@ -158,6 +201,6 @@ VALUES ('Felino');
 INSERT INTO petshop_api.breed (name, fk_id_species)
 VALUES ('Pastor Alemao', 1);
 
-INSERT INTO petshop_api.pet (name, date_created, date_birthday, fk_id_customer, fk_id_breed)
-VALUES ('Rex', now(), to_date('12/12/2016', 'dd/MM/yyyy'), 1, 1);
+INSERT INTO petshop_api.pet (name, date_created, date_birthday, fk_id_customer, fk_id_breed, fk_id_contract)
+VALUES ('Rex', now(), to_date('12/12/2016', 'dd/MM/yyyy'), 1, 1, 2);
 
